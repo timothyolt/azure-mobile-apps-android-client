b7c01e558fc281afc6f6fae9b666b625cb31007e
Timothy Oltjenbruns <timothyolt@gmail.com>
2018-01-19 20:23:34 -0500

    Fixes #126 by checking JsonArray elements before casting to JsonObjects

--- sdk/src/sdk/src/main/java/com/microsoft/windowsazure/mobileservices/table/serialization/JsonEntityParser.java
+++ sdk/src/sdk/src/main/java/com/microsoft/windowsazure/mobileservices/table/serialization/JsonEntityParser.java
@@ -52,7 +52,9 @@ public class JsonEntityParser {
             JsonArray elements = results.getAsJsonArray();
 
             for (JsonElement element : elements) {
-                changeIdPropertyName(element.getAsJsonObject(), idPropertyName);
+                if (results.isJsonObject()) {
+                    changeIdPropertyName(element.getAsJsonObject(), idPropertyName);
+                }
                 E typedElement = gson.fromJson(element, clazz);
                 result.add(typedElement);
             }
